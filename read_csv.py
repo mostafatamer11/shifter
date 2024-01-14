17c56b2db7f4fc3f9be9888138396beb26515441
Mostafa Tamer <mostafaghobashytamer@gmail.com>
2024-01-14 19:26:38 +0200

    initial commit

--- read_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("input.csv")
-
-COLUMNS = [df.columns[x].strip() for x in range(len(df.columns))]
-LOW_COLUMNS = [df.columns[x].lower().strip() for x in range(len(df.columns))]
-
